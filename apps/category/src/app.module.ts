4ec4e529f937ebcff4bf433d1ac361a769c5e7cb
itznik79 <nikhilkgautam97@gmail.com>
2026-02-26 23:27:25 +0530

    feat(category): commit category service changes

--- apps/category/src/app.module.ts
+++ apps/category/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MonitoringModule } from '../../../packages/monitoring/dist/monitoring.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '@app/database';
 import { Category } from './modules/category/category.model';
@@ -12,6 +13,7 @@ import { CategoryModule } from './modules/category/category.module';
             models: [Category]
         }),
         CategoryModule,
+        MonitoringModule.forRoot({ sentry: { dsn: process.env.SENTRY_DSN, env: process.env.NODE_ENV }, metrics: true }),
     ],
 })
 export class AppModule { }
